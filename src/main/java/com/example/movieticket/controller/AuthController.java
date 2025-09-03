fe76e90ebbd689fe458036b06aa2f7be476b08c2
Prakhar <prakharcanoe@gmail.com>
2025-09-03 05:56:46 +0530

    fixed registration

--- src/main/java/com/example/movieticket/controller/AuthController.java
+++ src/main/java/com/example/movieticket/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class AuthController {
 
     private final UserRepository userRepository;
